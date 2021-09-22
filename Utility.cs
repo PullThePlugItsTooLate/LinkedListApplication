8b1af4a731b0b05db76fbeeb6c1311c470c715d2
Suvashin Moodliar <suvashins@gmail.com>
2021-09-22 23:22:01 +0300

    Add project files.

---
+++ Utility.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListApplication
+{
+    class Utility
+    {
+        public static LinkedList<string> llstMovies = new LinkedList<string>();
+
+        public static void addMovies() 
+        {
+            llstMovies.AddLast("Titanic");
+        }
+
+        internal void InsertFront(SingleLinkedList singlyList, int new_data)
+        {
+            Node new_node = new Node(new_data);
+            new_node.next = singlyList.head;
+            singlyList.head = new_node;
+        }
+
+        internal void InsertLast(SingleLinkedList singlyList, int new_data)
+        {
+            Node new_node = new Node(new_data);
+            if (singlyList.head == null)
+            {
+                singlyList.head = new_node;
+                return;
+            }
+            Node lastNode = GetLastNode(singlyList);
+            lastNode.next = new_node;
+        }
+
+
+        internal Node GetLastNode(SingleLinkedList singlyList)
+        {
+            Node temp = singlyList.head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+
+
+        internal void InsertAfter(Node prev_node, int new_data)
+        {
+            if (prev_node == null)
+            {
+                Console.WriteLine("The given previous node Cannot be null");
+                return;
+            }
+            Node new_node = new Node(new_data);
+            new_node.next = prev_node.next;
+            prev_node.next = new_node;
+        }
+
+        internal void DeleteNodebyKey(SingleLinkedList singlyList, int key)
+        {
+            Node temp = singlyList.head;
+            Node prev = null;
+            if (temp != null && temp.data == key)
+            {
+                singlyList.head = temp.next;
+                return;
+            }
+            while (temp != null && temp.data != key)
+            {
+                prev = temp;
+                temp = temp.next;
+            }
+            if (temp == null)
+            {
+                return;
+            }
+            prev.next = temp.next;
+        }
+
+
+        public void ReverseLinkedList(SingleLinkedList singlyList)
+        {
+            Node prev = null;
+            Node current = singlyList.head;
+            Node temp = null;
+            while (current != null)
+            {
+                temp = current.next;
+                current.next = prev;
+                prev = current;
+                current = temp;
+            }
+            singlyList.head = prev;
+        }
+
+
+    }
+}
